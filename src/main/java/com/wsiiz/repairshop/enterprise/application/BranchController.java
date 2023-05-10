cddaf961e49a3ec639883751cf55da8138e272bc
dominik <dominik_wodzien@o2.pl>
2023-05-10 21:23:20 +0200

    Poprawka_5

--- src/main/java/com/wsiiz/repairshop/enterprise/application/BranchController.java
+++ src/main/java/com/wsiiz/repairshop/enterprise/application/BranchController.java
@@ -1,47 +1,57 @@
 package com.wsiiz.repairshop.enterprise.application.branch;
 
+import com.wsiiz.repairshop.enterprise.domain.employee.Employee;
 import com.wsiiz.repairshop.enterprise.domain.branch.Branch;
-import com.wsiiz.repairshop.enterprise.domain.branch.BranchRepository;
-import com.wsiiz.repairshop.enterprise.domain.employee.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/branches")
+@Controller
+@RequestMapping(path="/branches")
 public class BranchController {
 
     @Autowired
-    private BranchRepository branchRepository;
+    private Branch branch;
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private Employee employeeRepository;
+
+    @GetMapping
+    public String getAllBranches(Model model) {
+        model.addAttribute("branches", branch.findAll());
+        return "branch-list";
+    }
 
     @GetMapping("/{id}")
-    public Branch getBranchById(@PathVariable Long id) {
-        return branchRepository.findById(id).orElse(null);
+    public String getBranchById(@PathVariable Long id, Model model) {
+        model.addAttribute("branch", branch.findById(id).orElse(null));
+        return "branch-details";
+    }
+
+    @GetMapping("/new")
+    public String newBranch(Model model) {
+        model.addAttribute("branch", new Branch());
+        model.addAttribute("employees", employee.findAll());
+        return "branch-form";
     }
 
     @PostMapping
-    public Branch createBranch(@RequestBody Branch branch) {
-        return branchRepository.save(branch);
+    public String saveOrUpdateBranch(@ModelAttribute Branch branch) {
+        branchRepository.save(branch);
+        return "redirect:/branches";
     }
 
-    @PutMapping("/{id}")
-    public Branch updateBranch(@PathVariable Long id, @RequestBody Branch branch) {
-        Branch existingBranch = branchRepository.findById(id).orElse(null);
-        if (existingBranch != null) {
-            existingBranch.setName(branch.getName());
-            existingBranch.setAddress(branch.getAddress());
-            existingBranch.setBranchType(branch.getBranchType());
-            existingBranch.getEmployees().clear();
-            existingBranch.getEmployees().addAll(branch.getEmployees());
-            return branchRepository.save(existingBranch);
-        }
-        return null;
+    @GetMapping("/edit/{id}")
+    public String editBranch(@PathVariable Long id, Model model) {
+        model.addAttribute("branch", branchRepository.findById(id).orElse(null));
+        model.addAttribute("employees", employeeRepository.findAll());
+        return "branch-form";
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteBranch(@PathVariable Long id) {
+    @GetMapping("/delete/{id}")
+    public String deleteBranch(@PathVariable Long id) {
         branchRepository.deleteById(id);
+        return "redirect:/branches";
     }
 }
--- src/main/java/com/wsiiz/repairshop/enterprise/application/EmployeeController.java
+++ src/main/java/com/wsiiz/repairshop/enterprise/application/EmployeeController.java
@@ -1,27 +1,54 @@
 package com.wsiiz.repairshop.enterprise.application.employee;
 
-import com.wsiiz.repairshop.enterprise.domain.branch.Branch;
 import com.wsiiz.repairshop.enterprise.domain.employee.Employee;
 import com.wsiiz.repairshop.enterprise.domain.employee.EmployeeRepository;
+import com.wsiiz.repairshop.enterprise.domain.branch.BranchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Set;
-
+@Controller
+@RequestMapping(path="/employees")
 public class EmployeeController {
 
-    private final EmployeeRepository employeeRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    @Autowired
+    private BranchRepository branchRepository;
+
+    @GetMapping
+    public String getAllEmployees(Model model) {
+        model.addAttribute("employees", employeeRepository.findAll());
+        return "employee-list";
     }
 
-    public void assignEmployeeToBranch(Long employeeId, Long branchId) {
-        Employee employee = employeeRepository.findById(employeeId);
-        Branch branch = branchRepository.findById(branchId);
-        employee.setBranch(branch);
+    @GetMapping("/{id}")
+    public String getEmployeeById(@PathVariable Long id, Model model) {
+        model.addAttribute("employee", employeeRepository.findById(id).orElse(null));
+        return "employee-details";
+    }
+
+    @GetMapping("/new")
+    public String newEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("branches", branchRepository.findAll());
+        return "employee-form";
+    }
+
+    @PostMapping
+    public String saveOrUpdateEmployee(@ModelAttribute Employee employee) {
         employeeRepository.save(employee);
+        return "redirect:/employees";
     }
 
-    public Set<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+    @GetMapping("/edit/{id}")
+    public String editEmployee(@PathVariable Long id, Model model) {
+        model.addAttribute("employee", employeeRepository.findById(id).orElse(null));
+        model.addAttribute("branches", branchRepository.findAll());
+        return "employee-form";
     }
-}
+
+    @GetMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
